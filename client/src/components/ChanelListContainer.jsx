a298e0c22f26246291daee5375d49649a6b814ca
arod147 <code.alex.rodriguez@outlook.com>
2022-03-30 23:13:46 -0400

    Search bar done

--- client/src/components/ChanelListContainer.jsx
+++ client/src/components/ChanelListContainer.jsx
@@ -65,7 +65,7 @@ const ChanelListContent = ({
       <SideBar logout={logout} />
       <div className="channel-list__list__wrapper">
         <CompanyHeader />
-        <ChannelSearch />
+        <ChannelSearch setToggleContainer={setToggleContainer} />
         <ChannelList
           filters={filters}
           channelRenderFilterFn={customChannelTeamFilter}
